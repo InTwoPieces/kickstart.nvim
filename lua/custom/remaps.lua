510b7c787c692b507d03ade9045b5de9e993ff63
Stefan <stefankaiserdn01@gmail.com>
2026-05-16 12:28:12 +0200

    added remaps

---
+++ lua/custom/remaps.lua
@@ -0,0 +1,22 @@
+vim.keymap.set("n", "<leader>ex", vim.cmd.Ex)
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+vim.keymap.set("x", "<leader>p", "\"_dP")
+
+vim.keymap.set("i", "jj", "<esc>")
+
+vim.keymap.set("n", "<leader>y", "\"+y")
+vim.keymap.set("v", "<leader>y", "\"+y")
+vim.keymap.set("n", "<leader>Y", "\"+Y")
+
+vim.keymap.set("n", "Q", "<nop>")
+
+vim.keymap.set("n", "x", '"_x')
+
+vim.keymap.set("v", "<", "<gv")
+vim.keymap.set("v", ">", ">gv")
